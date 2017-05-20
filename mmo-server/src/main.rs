ab4ad0294a9318d080aecad92d80f620c854b8f9
Alex Rozgo <alex.rozgo@gmail.com>
2017-05-19 18:13:47 -0700

    cmd args and pub sub

--- mmo-server/src/main.rs
+++ mmo-server/src/main.rs
@@ -1,21 +1,17 @@
-//! An UDP echo server that just sends back everything that it receives.
-//!
-//! If you're on unix you can test this out by in one terminal executing:
-//!
-//!     cargo run --example echo-udp
-//!
-//! and in another terminal you can run:
+//! Test with:
 //!
 //!     nc -4u localhost 8080
-//!
-//! Each line you type in to the `nc` terminal should be echo'd back to you!
 
+extern crate clap;
 extern crate futures;
 #[macro_use]
 extern crate tokio_core;
 
-use std::{env, io};
+use clap::{Arg, App};
+
+use std::{io};
 use std::net::SocketAddr;
+use std::collections::HashSet;
 
 use futures::{Future, Poll};
 use tokio_core::net::UdpSocket;
@@ -24,6 +20,7 @@ use tokio_core::reactor::Core;
 struct Server {
     socket: UdpSocket,
     buf: Vec<u8>,
+    peers: HashSet<SocketAddr>,
     to_send: Option<(usize, SocketAddr)>,
 }
 
@@ -33,38 +30,56 @@ impl Future for Server {
 
     fn poll(&mut self) -> Poll<(), io::Error> {
         loop {
-            // First we check to see if there's a message we need to echo back.
-            // If so then we try to send it back to the original source, waiting
-            // until it's writable and we're able to do so.
+
             if let Some((size, peer)) = self.to_send {
-                let amt = try_nb!(self.socket.send_to(&self.buf[..size], &peer));
-                println!("Echoed {}/{} bytes to {}", amt, size, peer);
+                for recv in self.peers.iter().filter(|&&x| x != peer) {
+                    try_nb!(self.socket.send_to(&self.buf[..size], recv));
+                    // let amt = try_nb!(self.socket.send_to(&self.buf[..size], recv));
+                    // println!("Echoed {}/{} bytes to {}", amt, size, peer);
+                }
                 self.to_send = None;
             }
 
-            // If we're here then `to_send` is `None`, so we take a look for the
-            // next message we're going to echo back.
-            self.to_send = Some(try_nb!(self.socket.recv_from(&mut self.buf)));
+            let (size, peer) = try_nb!(self.socket.recv_from(&mut self.buf));
+            self.peers.insert(peer);
+            
+            self.to_send = Some((size, peer));
+            
+            // if let Some((size, peer)) = self.to_send {
+            //     //let sparkle_heart = String::from_utf8(self.buf).unwrap();
+            //     // println!("got: {:?} from: {}", self.buf, peer);
+            // }
         }
     }
 }
 
 fn main() {
-    let addr = env::args().nth(1).unwrap_or("138.68.41.91:8080".to_string());
+
+    let matches =
+        App::new("mmo-server")
+            .version("0.1.0")
+            .about("Simulates a universe!")
+            .author("Alex Rozgo")
+            .arg(Arg::with_name("addr")
+                .short("a")
+                .long("address")
+                .help("Host to connect to address:port")
+                .takes_value(true))
+            .get_matches();
+
+    let addr = matches.value_of("addr").unwrap_or("127.0.0.1:8080");
     let addr = addr.parse::<SocketAddr>().unwrap();
 
-    // Create the event loop that will drive this server, and also bind the
-    // socket we'll be listening to.
     let mut l = Core::new().unwrap();
     let handle = l.handle();
     let socket = UdpSocket::bind(&addr, &handle).unwrap();
     println!("Listening on: {}", addr);
 
-    // Next we'll create a future to spawn (the one we defined above) and then
-    // we'll run the event loop by running the future.
     l.run(Server {
         socket: socket,
         buf: vec![0; 1024],
+        peers: HashSet::new(),
         to_send: None,
     }).unwrap();
 }
+
